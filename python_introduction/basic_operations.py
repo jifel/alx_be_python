bdd9c5068f414f04cc978a060baaa5c26a871800
elorm <jiffaelorm@gmail.com>
2025-05-23 23:55:33 +0000

    Add basic operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+# performing operations
+addition = number1 + number2
+sub = number1 - number2
+mul = number1*number2
+
+# Printing the results
+
+print("Addition of", number1, "and", number2,  "is", addition )
+print("Subtraction of", number1, "and", number2,  "is", sub )
+print("Multiplication of",  number1, "and", number2, "is", mul )
+
+
+
