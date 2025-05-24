e70d88a19a6ed0ae564fdf6f440fc3220b750f7a
elorm <jiffaelorm@gmail.com>
2025-05-24 00:05:29 +0000

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# defining 3 variables 
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculating the simple interest, Interest = p*r*t
+
+interest = principal * rate * time
+
+#printing the simple interest
+
+print("The simple interest is:", interest)
