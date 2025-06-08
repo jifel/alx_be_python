f48cfae6f67c82a50148de17df2aa2136e0e6a66
elorm <jiffaelorm@gmail.com>
2025-06-08 21:39:29 +0000

    Update prompt name to match checker

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -35,10 +35,10 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            add_item = input("Enter an item to add to the list: ")
+            add_item = input("Enter the  item to add: ")
             shopping_list.append(add_item)
             pass
-        
+
         elif choice == '2':
             # Prompt for and remove an item
             remove_item = input("Enter an item to be removed from the list: ")
